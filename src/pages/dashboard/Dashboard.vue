dd9956fa8f7779d8e365fcf03c4910c72d512deb
夏彬阳 <xby020@163.com>
2023-11-16 16:39:13 +0800

    feat(screen): update transform

--- src/pages/dashboard/Dashboard.vue
+++ src/pages/dashboard/Dashboard.vue
@@ -2,7 +2,7 @@
   <div
     class="w-full h-full flex justify-center items-center text-2xl font-extralight"
   >
-    This is start page,write down your first page content.
+    
   </div>
 </template>
 
